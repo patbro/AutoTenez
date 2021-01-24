3f0874a27d45cc4338df0a07f34cae2e89588d29
Patrick van Broeckhuijsen <github@patb.ro>
2021-01-24 15:19:12 +0100

    Handle local and serve time difference, make reservation for multiple time slots, add sanity checks, improve logging

--- AutoTenez.py
+++ AutoTenez.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from datetime import date
 import datetime
+from datetime import date, datetime
+from datetime import timedelta
 import time
 import os
 import json
@@ -9,7 +10,8 @@ import sys
 
 ###################################################
 
-only_retrieve_clubapp_id = False
+only_retrieve_clubapp_id = False # Set to True to retrieve your clubapp ID, then set to False again
+only_check_time_slots_dont_make_reservation = False # Dry run functionality. False by default
 
 email_address = "" # Your email address
 password = "" # Your password in plain-text
@@ -17,16 +19,15 @@ password = "" # Your password in plain-text
 your_clubapp_id = "" # Your own clubapp ID
 friend_clubapp_id = "" # Clubapp ID of friend who you are reserving the court with
 
-first_choice_first_hour = "08:30" 
-first_choice_second_hour = "09:30" # Either specify a time or None
-first_choice_courts = ["Baan 1", "Baan 2", "Baan 3", "Baan 4"]
+reservation_date = "2021-01-25" # Specify the date to make the reservation (yyyy-mm-dd)
 
-second_choice_first_hour = "08:30"
-second_choice_second_hour = None # Either specify a time or None
-second_choice_courts = [] # Either specify courts or []
+first_choice_first_hour = "08:30" # Either specify a time or None (hh:mm)
+first_choice_second_hour = "09:30" # Either specify a time or None (hh:mm)
+first_choice_courts = ["Baan 1", "Baan 2", "Baan 3", "Baan 4"] # Either specify courts ("Baan X", where X is the court number) or []
 
-first_choice_preference = False # False: will find the longest available time slot. Might be the first or second choice.
-                                # True:  will find the best firt option available. Might not be both time slots (so only one hour).
+second_choice_first_hour = "08:00" # Either specify a time or None (hh:mm)
+second_choice_second_hour = None # Either specify a time or None (hh:mm)
+second_choice_courts = [] # Either specify courts ("Baan X", where X is the court number) or []
 
 # Usually you don't have to mess with the cookies, rather than just eating them
 cookies = "AWSELB=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F3430930403132013A6DD48D61EC856130104D8A52983E53940E17D1F9E11CA9FA6416D564EE3AF294; AWSELBCORS=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F3430930403132013A6DD48D61EC856130104D8A52983E53940E17D1F9E11CA9FA6416D564EE3AF294"
@@ -37,16 +38,16 @@ cookies = "AWSELB=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F34309
 
 def find_time_slot(available_slots, first_hour, second_hour=None, courts=[]):
     previous_slot_available_and_matches = False
-    both_time_slots_found = False
-    single_time_slot_found = False
+    second_time_slot_found = False
+    first_time_slot_found = False
 
     # Sanity checks
     if (len(available_slots) < 1):
-        print("No slots available")
+        print(" - No slots available")
         return
-    if (first_hour == None):
-        print("First hour is invalid")
-        return
+    if (first_hour is None):
+        print(" - First hour is None. Skip")
+        return 0, False, False
 
     # Loop through all available slots to find a match
     for slot in available_slots:
@@ -54,37 +55,42 @@ def find_time_slot(available_slots, first_hour, second_hour=None, courts=[]):
         time_slot = slot[1][11:16]
         md5slotkey = slot[2]
 
+        # Compensate for difference between local time and server time
+        time_slot = format(datetime.strptime(time_slot, '%H:%M') + timedelta(hours=+1), '%H:%M')
+
         # If we already found the first hour, check if this time slot also matches the second hour
         if (previous_slot_available_and_matches == True) and (second_hour == time_slot):
             # Yay! A court is available! Check if desired court matches
             if (check_court(courts, court_name) == True):
-                print("Voor je gekozen tijdslot om " + first_hour + " en " + second_hour + " is " + court_name + " nog vrij met md5slotkey " + md5slotkey)
-                both_time_slots_found = md5slotkey
+                print(" - For the chosen time slot the first hour " + first_hour + " and second hour " + second_hour + " are available on " + court_name + " (" + md5slotkey + ")")
+                second_time_slot_found = md5slotkey
                 break
 
         # Check if this time slot matches the first hour
         if (first_hour == time_slot):
             # A court (for at least the first hour) is available! Check if desired court matches
             if (check_court(courts, court_name) == True):
-                single_time_slot_found = md5slotkey
+                first_time_slot_found = md5slotkey
                 # Did the user specify a second hour?
                 if (second_hour == None):
-                    print("Voor je gekozen tijdslot om " + first_hour + " is " + court_name + " nog vrij met md5slotkey " + md5slotkey)
+                    print(" - For the chosen time slot the first hour " + first_hour + " is available on " + court_name + " (" + md5slotkey + ")")
                     break
                 else:
                     # Yes, they did. So continue searching...
                     previous_slot_available_and_matches = True
+                    print(" - At least time slot " + first_hour + " is available on " + court_name + " (" + md5slotkey + ")")
 
-    if (both_time_slots_found != False):
+    if (second_time_slot_found != False):
         # Reserve both time slot
-        return 2, both_time_slots_found
+        return 2, first_time_slot_found, second_time_slot_found
     
-    if (single_time_slot_found != False):
+    if (first_time_slot_found != False):
         # Reserve single time slot. Both time slots were not available
-        return 1, single_time_slot_found
+        return 1, first_time_slot_found, False
 
     # Unfortunately, no matching time slots were found
-    return 0, False
+    print(" - None found")
+    return 0, False, False
 
 
 def check_court(courts, court_name):
@@ -100,7 +106,40 @@ def check_court(courts, court_name):
 
     return False
 
+def make_reservation(bearer_token, date_tomorrow, md5slotkey, your_clubapp_id, friend_clubapp_id):
+    time.sleep(1) # Lets not stress the server too much
+    make_reservation_tomorrow_cli_cmd = "curl -i -s -k -X $'GET' \
+        -H $'Host: api.socie.nl' -H $'AppBundle: nl.tizin.socie.tennis' -H $'Accept: application/json' -H $'Authorization: bearer " + bearer_token + "' -H $'appVersion: 3.11.0' -H $'Accept-Language: en-us' -H $'Cache-Control: no-cache' -H $'Platform: iOS' -H $'membership_id: 5d635eee1ea4c97b221c58fc' -H $'Language: en-NL' -H $'Accept-Encoding: gzip, deflate' -H $'User-Agent: ClubApp/237 CFNetwork/1209 Darwin/20.2.0' -H $'Connection: close' -H $'Content-Type: application/json' \
+        -b $'" + cookies + "' \
+        $'https://api.socie.nl/communities/5a250a75d186db12a00f1def/tennis_court_reservation_create?date=" + str(date_tomorrow) + "&md5slotkey=" + md5slotkey + "&externalReferences=" + your_clubapp_id + "," + friend_clubapp_id + ",' 2>&1 | tee AutoTenezOutputReservation.txt"
+
+    output = os.popen(make_reservation_tomorrow_cli_cmd).read()
+    # A reponse with no content is returned if the reservation was successful
+    valid_output = "HTTP/1.1 204 No Content"
+    if (output.find(valid_output) == 0):
+        print("Successfully made the reservation! With md5slotkey " + md5slotkey)
+    else:
+        print("Failed to make the reservation with md5slotkey " + md5slotkey)
+
+###############
+
+# Sanity checks
+if (not email_address) or (not password):
+    print("ERROR! Enter your email address and password")
+    sys.exit(0)
+
+if (only_retrieve_clubapp_id == False) and ((not your_clubapp_id) or (not friend_clubapp_id)):
+    print("ERROR! Fill out the Clubapp IDs")
+    sys.exit(0)
+
+# Exit script if tomorrow is not the chosen date yet, so wait to make the reservation
+date_tomorrow = date.today() + timedelta(days=1)
+if (str(date_tomorrow) != reservation_date):
+    print("INFO! Chosen date (" + reservation_date + ") is not yet tomorrow ("+ str(date_tomorrow) +").")
+    sys.exit(0)
+
 # First of all, login 
+print("Logging in...")
 login_cli_cmd = "curl -i -s -k -X $'POST' \
     -H $'Host: api.socie.nl' -H $'AppBundle: nl.tizin.socie.tennis' -H $'Accept: application/json' -H $'appVersion: 3.11.0' -H $'Accept-Language: en-us' -H $'Cache-Control: no-cache' -H $'Platform: iOS' -H $'Accept-Encoding: gzip, deflate' -H $'Language: en-NL' -H $'Content-Length: 83' -H $'User-Agent: ClubApp/237 CFNetwork/1209 Darwin/20.2.0' -H $'Connection: close' -H $'Content-Type: application/json' \
     -b $'AWSELB=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F3430930403132013A6DD48D61EC856130104D8A52983E53940E17D1F9E11CA9FA6416D564EE3AF294; AWSELBCORS=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F3430930403132013A6DD48D61EC856130104D8A52983E53940E17D1F9E11CA9FA6416D564EE3AF294' \
@@ -126,6 +165,7 @@ f.write(bearer_token)
 f.close()
 
 if (only_retrieve_clubapp_id == True):
+    print("Retrieving your clubapp ID...")
     # Retrieve user ID, so we can make another call to retrieve your external reference (i.e. clubapp ID)
     decoded = jwt.decode(bearer_token, options={"verify_signature": False})
     user_id = str(decoded)[166:190] # No comment...
@@ -150,8 +190,7 @@ if (only_retrieve_clubapp_id == True):
     sys.exit(0)
 
 # Retrieve slots for tomorrow
-date_tomorrow = date.today() + datetime.timedelta(days=1)
-# Prepare request to perform
+print("Retrieving time slots for tomorrow...")
 get_slots_tomorrow_cli_cmd = "curl -i -s -k -X $'GET' \
     -H $'Host: api.socie.nl' -H $'AppBundle: nl.tizin.socie.tennis' -H $'Accept: application/json' -H $'Authorization: bearer " + bearer_token + "' -H $'appVersion: 3.11.0' -H $'Accept-Language: en-us' -H $'Cache-Control: no-cache' -H $'Platform: iOS' -H $'membership_id: 5d635eee1ea4c97b221c58fc' -H $'Language: en-NL' -H $'Accept-Encoding: gzip, deflate' -H $'User-Agent: ClubApp/237 CFNetwork/1209 Darwin/20.2.0' -H $'Connection: close' -H $'Content-Type: application/json' \
     -b $'AWSELB=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F3430930403132013A6DD48D61EC856130104D8A52983E53940E17D1F9E11CA9FA6416D564EE3AF294; AWSELBCORS=57C90D931ED7A0D581FA0FDCC1A541BAF664D2A7C6ADF011E1622185F3430930403132013A6DD48D61EC856130104D8A52983E53940E17D1F9E11CA9FA6416D564EE3AF294' \
@@ -184,41 +223,42 @@ for court_no in range(0, 12):
             available_slots.append(slot)
 
 # Find available time slots
-first_choice_no_slots, first_choice_slotkey = find_time_slot(available_slots, first_choice_first_hour, first_choice_second_hour, first_choice_courts)
-second_choice_no_slots, second_choice_slotkey = find_time_slot(available_slots, second_choice_first_hour, second_choice_second_hour, second_choice_courts)
+print("Finding available time slots for your first choice...")
+first_choice_no_slots, first_choice_first_slotkey, first_choice_second_slotkey = find_time_slot(available_slots, first_choice_first_hour, first_choice_second_hour, first_choice_courts)
+print("Finding available time slots for your second choice...")
+second_choice_no_slots, second_choice_first_slotkey, second_choice_second_slotkey = find_time_slot(available_slots, second_choice_first_hour, second_choice_second_hour, second_choice_courts)
 
-md5slotkey = ""
-if (first_choice_preference == True):
-    # No matter how big the time slot is, reserve the first choice
-    if (first_choice_slotkey != False):
-        print("Reserveer eerste keuze: aantal sloten " + str(first_choice_no_slots) + " - " + first_choice_slotkey)
-        md5slotkey = first_choice_slotkey
-    elif(second_choice_slotkey != False):
-        print("Reserveer tweede keuze: aantal sloten " + str(second_choice_no_slots) + " - " + second_choice_slotkey)
-        md5slotkey = second_choice_slotkey
-    else:
-        print("Geen tijds sloten beschikbaar")
+first_md5slotkey = ""
+second_md5slotkey = ""
+
+# Reserve the biggest time slot
+if(second_choice_no_slots > first_choice_no_slots):
+    print("Propagating second choice. Number of time slots: " + str(second_choice_no_slots))
+    if (second_choice_first_slotkey):
+        print(" - First time slot md5slotkey " + second_choice_first_slotkey)
+        first_md5slotkey = second_choice_first_slotkey
+    if (second_choice_second_slotkey):
+        print(" - Second time slot md5slotkey " + second_choice_second_slotkey)
+        second_md5slotkey = second_choice_second_slotkey
+elif(first_choice_first_slotkey != False):
+    print("Propagating first choice. Number of time slots: " + str(first_choice_no_slots))
+    if (first_choice_first_slotkey):
+        print(" - First time slot md5slotkey " + first_choice_first_slotkey)
+        first_md5slotkey = first_choice_first_slotkey
+    if (first_choice_second_slotkey):
+        print(" - Second time slot md5slotkey " + first_choice_second_slotkey)
+        second_md5slotkey = first_choice_second_slotkey
 else:
-    # Reserve the biggest time slot
-    if(second_choice_no_slots > first_choice_no_slots):
-        print("Reserveer tweede keuze: aantal sloten " + str(second_choice_no_slots) + " - " + second_choice_slotkey)
-        md5slotkey = second_choice_slotkey
-    elif(first_choice_slotkey != False):
-        print("Reserveer eerste keuze: aantal sloten " + str(first_choice_no_slots) + " - " + first_choice_slotkey)
-        md5slotkey = first_choice_slotkey
-    else:
-        print("Geen tijds sloten beschikbaar")
+    print("There are no time slots available")
 
-if (md5slotkey):
-    make_reservation_tomorrow_cli_cmd = "curl -i -s -k -X $'GET' \
-        -H $'Host: api.socie.nl' -H $'AppBundle: nl.tizin.socie.tennis' -H $'Accept: application/json' -H $'Authorization: bearer " + bearer_token + "' -H $'appVersion: 3.11.0' -H $'Accept-Language: en-us' -H $'Cache-Control: no-cache' -H $'Platform: iOS' -H $'membership_id: 5d635eee1ea4c97b221c58fc' -H $'Language: en-NL' -H $'Accept-Encoding: gzip, deflate' -H $'User-Agent: ClubApp/237 CFNetwork/1209 Darwin/20.2.0' -H $'Connection: close' -H $'Content-Type: application/json' \
-        -b $'" + cookies + "' \
-        $'https://api.socie.nl/communities/5a250a75d186db12a00f1def/tennis_court_reservation_create?date=" + str(date_tomorrow) + "&md5slotkey=" + md5slotkey + "&externalReferences=" + your_clubapp_id + "," + friend_clubapp_id + ",' 2>&1 | tee AutoTenezOutputReservation.txt"
+if (only_check_time_slots_dont_make_reservation):
+    print("Not making a reservation because dryrun is set to True")
+    sys.exit(0)
 
-    output = os.popen(make_reservation_tomorrow_cli_cmd).read()
-    valid_output = "HTTP/1.1 204 No Content"
+if (first_md5slotkey):
+    print("Make the reservation for the first time slot...")
+    make_reservation(bearer_token, date_tomorrow, first_md5slotkey, your_clubapp_id, friend_clubapp_id)
 
-    if (output.find(valid_output) == 0):
-        print("Reservering geplaatst!")
-    else:
-        print("Het is niet gelukt te reserveren")
+if (second_md5slotkey):
+    print("Make the reservation for the second time slot...")
+    make_reservation(bearer_token, date_tomorrow, second_md5slotkey, your_clubapp_id, friend_clubapp_id)
